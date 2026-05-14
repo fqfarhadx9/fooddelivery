85f4a5288c022c0c758c82cf453398ff8c93c967
Farhad Qaunain <fqqaunain@gmail.com>
2026-05-15 03:47:36 +0530

    initial commit

---
+++ client/src/utils/data.js
@@ -0,0 +1,56 @@
+export const category = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/736x/fd/4b/1e/fd4b1e9fbbee80cb8528fea54cddcb50.jpg",
+    name: "Burgers",
+    off: "20-40% OFF",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/premium-photo/huge-selection-peeroni-pizza-with-tomato-sauce-sausage-dark-table_124507-61025.jpg",
+    name: "Pizzas",
+    off: "10-20% OFF",
+  },
+  {
+    id: 3,
+    img: "https://imgs.search.brave.com/RFUGbCJKpWfBGsoFAYKCyMu5QtMdxyLXU1ObSbsC1j8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNzMv/OTE1LzY1MS9zbWFs/bC9kZWxpY2lvdXMt/Y2hpY2tlbi1iaXJ5/YW5pLXBsYXRlZC1y/ZWFkeS10by1lYXQt/cGhvdG8uanBn",
+    name: "Biriyanis",
+    off: "20-40% OFF",
+  },
+  {
+    id: 4,
+    img: "https://imgs.search.brave.com/ynkmeSp7AKE4-uKJySDn37pB5Tm0TgD196wUnZSFg-s/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzLzk0Yi9kZWxp/Y2lvdXMtc3RyYXdi/ZXJyeS10YXJ0LTA0/MTAtNTY5NzY2Ny5q/cGc_Zm10",
+    name: "Desserts",
+    off: "30-40% OFF",
+  },
+  {
+    id: 5,
+    img: "https://media.istockphoto.com/id/1366811190/photo/different-type-of-coktails-on-wooden-table.jpg?s=612x612&w=0&k=20&c=BS4lAi77xCxn7LkMleAkw56H-IBaSzE1P2tQuUJcQlQ=",
+    name: "Beverages",
+    off: "10-40% OFF",
+  },
+];
+
+export const filter = [
+  {
+    name: "Food Categories",
+    value: "category",
+    items: [
+      "Noodles",
+      "Burger",
+      "Pizza",
+      "Pasta",
+      "Sushi",
+      "Tacos",
+      "Salads",
+      "Sandwiches",
+      "Dessert",
+      "Beverages",
+    ],
+  },
+  {
+    name: "Filter by Price",
+    value: "price",
+    items: [],
+  },
+];
